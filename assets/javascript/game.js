a51f0eb3a9c406a2e5aef65573586f15494c1c82
Erik Cribley <49967226+erikcribley@users.noreply.github.com>
2019-05-27 13:22:41 -0400

    got letters to replace blanks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+//Create array of possible words
 var words = [
     "bard", 
     "barbarian",
@@ -12,29 +13,37 @@ var words = [
     "wizard",
     "warlock",]
 
+//Establish global vairiables
 var wins = 0;
-var blankSpace = [];
 var guesses = 6;
 var rightLetter = [];
 var wrongLetter = [];
-
 
 var wordDisplay = document.getElementById("word-display")
 var winCount = document.getElementById("win-count")
 var guessesLeft = document.getElementById("guesses-left")
 var lettersGuessed = document.getElementById("letters-guessed")
 
+//Choose word at random
 var randomWord = words[Math.floor(Math.random() * words.length)];
 
+//Display word with underscores
 var blanks = [];
     for (i = 0; i < randomWord.length; i++) {
         blanks[i] = ('_')
     }
 
-wordDisplay.innerHTML = blanks
-
 document.onkeyup = function(event) {
-    var userInput = event.key;
+    var guess = event.key
+    for (i = 0; i < randomWord.length; i++) {
+        if (guess === randomWord[i]) {
+           blanks[i] = guess
+        } 
+        wordDisplay.innerHTML = blanks 
+    }
 }
 
-console.log(randomWord)
+wordDisplay.innerHTML = blanks 
+winCount.innerHTML = wins
+guessesLeft.innerHTML = guesses
+lettersGuessed.innerHTML = wrongLetter
