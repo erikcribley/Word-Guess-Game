c8efe4279ea43d6da4bc15dd006cd844e5288155
Erik Cribley <49967226+erikcribley@users.noreply.github.com>
2019-05-30 22:37:32 -0400

    no guessing already missed letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,8 +11,7 @@ var words = [
     "rogue",
     "sorcerer",
     "wizard",
-    "warlock",
-]
+    "warlock"]
 
 //Create an array of acceptable inputs
 var chars = [
@@ -41,8 +40,7 @@ var chars = [
     "w",
     "x",
     "y",
-    "z",
-]
+    "z"]
 
 //Establish global variables
 var wins = 0;
@@ -73,7 +71,7 @@ function reset () {
     randomWord = words[Math.floor(Math.random() * words.length)];
     blanks = []
     for (i = 0; i < randomWord.length; i++) {
-    blanks[i] = ('_')
+        blanks[i] = ('_')
     }
     remainingGuesses = 6
     wrongLetter = ['-']
@@ -87,34 +85,35 @@ function game () {
         if (chars.indexOf(guess) < 0) {
             alert("Choose a lower case letter") 
         } else if (randomWord.indexOf(guess) > -1) {
-        for (i = 0; i < randomWord.length; i++) {
-            if (guess === randomWord[i]) { 
-            blanks[i] = guess;
-            lettersRemaining--;    
-        } }
+            for (i = 0; i < randomWord.length; i++) {
+                if (guess === randomWord[i]) { 
+                    blanks[i] = guess;
+                    lettersRemaining--;                
+                }    
+            }
         } else if (wrongLetter.indexOf('-') > -1) {
-        wrongLetter.pop()
-        wrongLetter.push(guess)
-        remainingGuesses--;
-        } else {
-        wrongLetter.push(guess)
-        remainingGuesses--;
+            wrongLetter.pop()
+            wrongLetter.push(guess)
+            remainingGuesses--;
+        } else if (wrongLetter.indexOf(guess) < 0) {
+            wrongLetter.push(guess)
+            remainingGuesses--;
         }
-        
+
         //Win condition and reset
-        if (lettersRemaining === 0) {
+        if (lettersRemaining=== 0) {
             wins++
             reset()
-            } else if (remainingGuesses=== 0) {
+        } else if (remainingGuesses=== 0) {
             reset()
-            }
+        }
 
     //Display in local scope
     wordDisplay.innerHTML = blanks.join(' ')
     guessesLeft.innerHTML = remainingGuesses
     lettersGuessed.innerHTML = wrongLetter.join(' ')
     winCount.innerHTML = wins
-    }      
+    }   
 }
 
 //Display in global scope
